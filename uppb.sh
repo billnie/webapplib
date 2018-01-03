43e921ffb6605495b8e8246582e64218292332d2
billnie <billnie@users.noreply.github.com>
2018-01-03 16:34:07 +0800

    change

--- uppb.sh
+++ uppb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #rm a.out
-g++ uppb.cpp hexdump.c  log_logstores.pb-c.c -lcgicc -lz -llz4 -lprotobuf-c -I/usr/include -I/usr/local/include/protobuf-c/
+#g++ uppb.cpp hexdump.c  log_logstores.pb-c.c -lcgicc -lz -llz4 -lprotobuf-c -I/usr/include -I/usr/local/include/protobuf-c/ -I/usr/local/include/webapplib -I/usr/include/mysql
+g++ uppb.cpp hexdump.c  log_logstores.pb-c.c -lcgicc -lz -llz4 -lprotobuf-c -lwebapp -I/usr/include -I/usr/local/include/protobuf-c/ -I/usr/local/include/webapplib -I/usr/include/mysql
 #g++ uppb.cpp hexdump.c  -lcgicc -lz -I/usr/include
 sudo cp a.out /var/www/cgi-bin/uppb.cgi
 #sudo cp a.out /Library/WebServer/CGI-Executables/uppb.cgi
