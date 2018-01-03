d5cf7b3628b7f5cf22c2ef5d35a78d7ffe1ac0a0
billnie <billnie@users.noreply.github.com>
2018-01-03 15:31:36 +0800

    change

--- uppb.sh
+++ uppb.sh
@@ -3,3 +3,4 @@
 g++ uppb.cpp hexdump.c  log_logstores.pb-c.c -lcgicc -lz -llz4 -lprotobuf-c -I/usr/include -I/usr/local/include/protobuf-c/
 #g++ uppb.cpp hexdump.c  -lcgicc -lz -I/usr/include
 sudo cp a.out /var/www/cgi-bin/uppb.cgi
+#sudo cp a.out /Library/WebServer/CGI-Executables/uppb.cgi
