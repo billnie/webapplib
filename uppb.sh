56d7ef5e82b34a34de97d3f765e85fdfa00b2a86
billnie <706919534@qq.com>
2018-01-03 16:45:58 +0800

    change complie script

--- uppb.sh
+++ uppb.sh
@@ -3,5 +3,5 @@
 #g++ uppb.cpp hexdump.c  log_logstores.pb-c.c -lcgicc -lz -llz4 -lprotobuf-c -I/usr/include -I/usr/local/include/protobuf-c/ -I/usr/local/include/webapplib -I/usr/include/mysql
 g++ uppb.cpp hexdump.c  log_logstores.pb-c.c -lcgicc -lz -llz4 -lprotobuf-c -lwebapp -I/usr/include -I/usr/local/include/protobuf-c/ -I/usr/local/include/webapplib -I/usr/include/mysql
 #g++ uppb.cpp hexdump.c  -lcgicc -lz -I/usr/include
-sudo cp a.out /var/www/cgi-bin/uppb.cgi
+ cp a.out /var/www/cgi-bin/uppb.cgi
 #sudo cp a.out /Library/WebServer/CGI-Executables/uppb.cgi
